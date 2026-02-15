48205760ea0cada70bdc1d27397b46e06faa42fd
Michael Owusu Prempeh <prempehm902@gmail.com>
2026-02-15 12:33:37 +0100

    Update variables.tf

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,66 +1,86 @@
-# AWS region
+##############################
+# AWS Provider
+##############################
+
 variable "region" {
   description = "AWS region to deploy resources"
   type        = string
 }
 
-# EC2 instance type
+##############################
+# EC2 Variables
+##############################
+
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
 }
 
-# EC2 AMI
 variable "ec2_ami" {
   description = "AMI ID for EC2 instance. Use a regional AMI or dynamically look it up"
   type        = string
 }
 
-# PostgreSQL database name
+variable "my_ip" {
+  description = "Your public IP address allowed for SSH, in CIDR format (e.g., 123.45.67.89/32)"
+  type        = string
+}
+
+variable "iam_instance_profile" {
+  description = "IAM instance profile for the EC2 instance (optional)"
+  type        = string
+  default     = null
+}
+
+##############################
+# RDS Variables
+##############################
+
 variable "db_name" {
   description = "Name of the PostgreSQL database"
   type        = string
 }
 
-# PostgreSQL master username
 variable "db_username" {
   description = "Master username for the RDS database"
   type        = string
 }
 
-# PostgreSQL master password (sensitive)
 variable "db_password" {
   description = "Master password for the RDS database"
   type        = string
   sensitive   = true
 }
 
-# RDS instance class
 variable "db_instance_class" {
   description = "RDS instance class type"
   type        = string
   default     = "db.t3.micro"
 }
 
-# Your public IP for SSH
-variable "my_ip" {
-  description = "Your public IP address allowed for SSH, in CIDR format (e.g., 123.45.67.89/32)"
+variable "db_subnet_group_name" {
+  description = "Name for RDS subnet group"
   type        = string
 }
 
-# SNS email for CloudWatch alerts
+##############################
+# SNS / Monitoring
+##############################
+
 variable "sns_email" {
   description = "Email address to receive CloudWatch SNS alerts"
   type        = string
 }
 
-# VPC CIDR
+##############################
+# VPC / Networking Variables
+##############################
+
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
 }
 
-# Public subnet CIDR and AZ
 variable "public_subnet_cidr" {
   description = "CIDR block for the public subnet"
   type        = string
@@ -71,7 +91,6 @@ variable "public_az" {
   type        = string
 }
 
-# Private subnets CIDR and AZs
 variable "private_subnet_1_cidr" {
   description = "CIDR block for private subnet 1"
   type        = string
@@ -89,11 +108,5 @@ variable "private_az_1" {
 
 variable "private_az_2" {
   description = "Availability zone for private subnet 2"
-  type        = string
-}
-
-# RDS subnet group name
-variable "db_subnet_group_name" {
-  description = "Name for RDS subnet group"
   type        = string
 }
