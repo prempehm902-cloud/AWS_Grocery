b35e3f0cc0c6241d0fae6b21e3dadc950dbaea1e
Michael Owusu Prempeh <prempehm902@gmail.com>
2026-02-15 12:20:59 +0100

    Update security.tf

--- infrastructure/modules/security.tf
+++ infrastructure/modules/security.tf
@@ -1,42 +1,54 @@
+# Variables
+variable "vpc_id" {
+  description = "VPC ID where the security group will be created"
+  type        = string
+}
 
-variable "vpc_id" {}
+variable "my_ip" {
+  description = "Your public IP address allowed for SSH, in CIDR format (e.g., 123.45.67.89/32)"
+  type        = string
+}
 
+# Security Group for the application
 resource "aws_security_group" "app_sg" {
-  vpc_id = var.vpc_id
+  name        = "app-sg"
+  description = "Allow SSH, HTTP, and Flask app traffic"
+  vpc_id      = var.vpc_id
 
+  # SSH ingress (restricted to your IP)
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  # HTTP ingress
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Flask app ingress
   ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port = 5000
-    to_port   = 5000
-    protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  # Allow all outbound traffic
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-
-##modules/security/outputs.tf
-
+# Outputs
 output "app_sg_id" {
   value = aws_security_group.app_sg.id
 }
-
