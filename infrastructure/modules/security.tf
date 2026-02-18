a14af713189adf142a467ec80b6dd85dbf29384e
Michael Owusu Prempeh <prempehm902@gmail.com>
2026-02-18 17:02:48 +0100

    Update security.tf

--- infrastructure/modules/security.tf
+++ infrastructure/modules/security.tf
@@ -1,22 +1,16 @@
+# ---------------------------
 # EC2 Security Group
+# ---------------------------
 resource "aws_security_group" "app1_sg" {
   name        = "app1-sg"
-  description = "Allow SSH, HTTP, Flask"
+  description = "Allow SSH, HTTP, and Flask traffic"
   vpc_id      = aws_vpc.main.id
 
-  # 🔐 Restrict SSH to YOUR IP
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = [var.my_ip]
-  }
-
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -26,24 +20,37 @@ resource "aws_security_group" "app1_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = merge(var.common_tags, { Name = "app1-sg" })
 }
 
+# ---------------------------
 # RDS Security Group
+# ---------------------------
 resource "aws_security_group" "rds_sg" {
-  name   = "joy-rds-sg"
-  vpc_id = aws_vpc.main.id
+  name        = "rds-sg"
+  description = "Allow PostgreSQL from EC2"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
     security_groups = [aws_security_group.app1_sg.id]
+    description     = "PostgreSQL access from EC2"
   }
 
   egress {
@@ -52,4 +59,6 @@ resource "aws_security_group" "rds_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = merge(var.common_tags, { Name = "rds-sg" })
 }
